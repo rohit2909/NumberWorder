c530a60c4dff07814df3ef3a8f2423c899ea6ee6
Rohit Pasrija <rohit.pasrija@hotmail.com>
2016-02-10 21:47:01 +1000

    1st version

---
+++ NumberWorder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberWorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+        }
+    }
+}
